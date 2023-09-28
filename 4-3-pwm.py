6e1d9b8b1f805a8149808fedfbee66b8a32344b8
Likhachev Aleksey <likhach2005@gmail.com>
2023-09-28 21:23:33 +0300

    обнова

--- 4-3-pwm.py
+++ 4-3-pwm.py
@@ -7,10 +7,14 @@ GPIO.setup(24, GPIO.OUT)
 try:
     while true:
         x = int(input("Введите число от 0 до 100: "))
-        p = GPIO.PWM(24, 1000)
-        p.start(x)
-        input("Enter чтобы завершить: ")
-        p.stop()
+        if x>100:
+            print('Слишком большое значение.')
+        else:
+            p = GPIO.PWM(24, 1000)
+            p.start(x)
+            print('Предполагаемое напряжение ',3.3*x/100)
+            input("Enter чтобы завершить: ")
+            p.stop()
 finally:
     GPIO.output(24, 0)
     GPIO.cleanup()
