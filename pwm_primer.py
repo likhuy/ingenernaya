4ea76123854499747b1a06ab2d558e1c7f64b33b
Likhachev Aleksey <144794644+likhuy@users.noreply.github.com>
2023-09-29 10:59:05 +0300

    Update pwm_primer.py

--- pwm_primer.py
+++ pwm_primer.py
@@ -12,3 +12,5 @@ GPIO.cleanup()
 # в 3 задании нужно будет подключить измерение нарпяжения к конденсатору и можно будет задавать на нем напряжение при
 # большой частоте 1000 (если частота будет не большой 50 то конденсатор будет разряжаться)
 # при изменении коэфицента можно будет регулировать напряжение (будет проценты*3.3
+#https://ant-lab.mipt.ru/education/get/get-students/-/blob/master/4-dac/dac.md?ref_type=heads#%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0-3-%D1%88%D0%B8%D0%BC
+#https://ant-lab.mipt.ru/education/get/get-students/-/blob/master/3-git/git.md?ref_type=heads#%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5-%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D1%8B-git
